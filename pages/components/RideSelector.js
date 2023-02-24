aee4e9734e9af0b7d9eb6621189486b290fcee3a
J Sinha <jsinha@192.168.1.2>
2023-02-24 19:57:53 +0530

    Refactor code for deployment

--- pages/components/RideSelector.js
+++ pages/components/RideSelector.js
@@ -1,33 +1,33 @@
 import React, { useEffect, useState } from 'react'
 import tw from "tailwind-styled-components"
 import CarItem from './CarItem'
-import { carList } from '../data/carList'
+import { carList } from '../../data/carList'
 
 
-const RideSelector = ({pickupCoords, dropoffCoords}) => {
-    const [rideDuration, setRideDuration] = useState(0)
+const RideSelector = ({ pickupCoords, dropoffCoords }) => {
+  const [rideDuration, setRideDuration] = useState(0)
 
-    // get ride duration from mapboxGL
-    useEffect(() => {
-        fetch(`https://api.mapbox.com/directions/v5/mapbox/driving/${pickupCoords[0]},${pickupCoords[1]};${dropoffCoords[0]}, ${dropoffCoords[1]}?access_token=pk.eyJ1IjoianNpbmhhIiwiYSI6ImNsZHB0a2NuMDBha24zd256a2tzajZpcWMifQ.1_v3E0Wq34usRJ4EmrcMCA`)
-        .then(res => res.json())
-        .then(data => {
-            if (data !== undefined && data.routes.length >= 1) {
-                setRideDuration(data.routes[0].duration / 1000)
-            }
-        })
-    }, [pickupCoords, dropoffCoords]);
+  // get ride duration from mapboxGL
+  useEffect(() => {
+    fetch(`https://api.mapbox.com/directions/v5/mapbox/driving/${pickupCoords[0]},${pickupCoords[1]};${dropoffCoords[0]}, ${dropoffCoords[1]}?access_token=pk.eyJ1IjoianNpbmhhIiwiYSI6ImNsZHB0a2NuMDBha24zd256a2tzajZpcWMifQ.1_v3E0Wq34usRJ4EmrcMCA`)
+      .then(res => res.json())
+      .then(data => {
+        if (data !== undefined && data.routes.length >= 1) {
+          setRideDuration(data.routes[0].duration / 1000)
+        }
+      })
+  }, [pickupCoords, dropoffCoords]);
 
   return (
     <Wrapper>
       <Title>Choose a ride, or swipe up for more</Title>
       <CarList>
         {
-            carList.map((car, index)=>{
-                return <CarItem key={index} imgUrl={car.imgUrl} service={car.service} rideDuration={rideDuration} multiplier={car.multiplier}></CarItem>
-            })
-        } 
-    </CarList>  
+          carList.map((car, index) => {
+            return <CarItem key={index} imgUrl={car.imgUrl} service={car.service} rideDuration={rideDuration} multiplier={car.multiplier}></CarItem>
+          })
+        }
+      </CarList>
     </Wrapper>
   )
 }
